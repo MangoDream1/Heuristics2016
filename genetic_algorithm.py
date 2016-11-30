918a32ad9fd02dc0c829611bc70f4aed32737428
MangoDream1 <axel.verheul@gmail.com>
2016-11-30 11:48:03 +0100

    Genetic algorithm now also mutates

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -10,8 +10,10 @@ def update_progress(workdone, text='Progress:'):
     if workdone == 1:
         print('\n')
 
-def genetic_algorithm(im, nPopulation, nGenerations):
+def genetic_algorithm(im, nPopulation, nGenerations, mutation_rate):
     print("Starting genetic algorithm...")
+
+    nLectures = len(im.lectures)
 
     # Start with random timetables
     while im.i != nPopulation:
@@ -43,26 +45,36 @@ def genetic_algorithm(im, nPopulation, nGenerations):
         for key, score in scores[:(nPopulation // 5)]:
             del im.iteration_dct[key]
 
-        # Parent selection
+        # Calculate number of parents
         parents = []
-
         nParents = nPopulation // 5 * 2
 
+        # Save deleted keys for reuse, then delete from scores
+        deleted_keys = [x[0] for x in scores[:(nPopulation // 5)]]
         del scores[:(nPopulation // 5)]
 
+        # Parent selection
         while len(parents) != nParents:
             key, score = choice(scores)
 
             if score > random():
                 parents.append(key)
 
-        # Reproduce
+                # Mutate
+                if random() > mutation_rate:
+                    dct = im.iteration_dct[key]
 
+                    random_lecture = dct[choice(range(nLectures))]
+
+                    # Change classroom, timeslot and day of random lecture
+                    random_lecture["day"] = randint(0, 4)
+                    random_lecture["timeslot"] = randint(0, 3)
+                    random_lecture["classroom"] = choice(im.classrooms).getId()
+
+        # Reproduce
         while parents:
             father = parents.pop()
             mother = parents.pop()
-
-            nLectures = len(im.lectures)
 
             intersect = int(random() * nLectures)
 
@@ -72,13 +84,13 @@ def genetic_algorithm(im, nPopulation, nGenerations):
             mother_lectures = [im.iteration_dct[mother][x]
                                 for x in range(intersect, nLectures)]
 
+            # Has to be numbered dict so that iteration_manager can read it
             child_lectures = {i: x for i, x in
                                 enumerate(father_lectures + mother_lectures)}
 
             im.applyChanges(child_lectures)
 
-
-            im.i += 1
+            im.i = deleted_keys.pop()
             im.addChanges(im.lectures)
 
         cGeneration += 1
@@ -94,9 +106,9 @@ def genetic_algorithm(im, nPopulation, nGenerations):
     compiled_changes = im.compileChanges(best_iteration[0])
     im.applyChanges(compiled_changes)
 
-    im.exportLectures("GA%sp%sg%s" % (best_iteration[1], nPopulation, 
-                                      nGenerations))
+    im.exportLectures("GA%sp%sg%sm%s" % (best_iteration[1], nPopulation,
+                                         nGenerations, mutation_rate))
 
     return im.lecture_dct
 
-genetic_algorithm(iteration_manager, 500, 100)
+genetic_algorithm(iteration_manager, 1000, 1000, 0.05)
