6186cb03a6a821e1a81c6c66d8401233c4735000
JessieStam <jessiestam@quicknet.nl>
2016-12-16 12:25:35 +0100

    Revert "Minor bug fix"
    This reverts commit 7c660be4bb69f2f74b416b3be97f222c07af78d4.

--- random_timetables.py
+++ random_timetables.py
@@ -19,20 +19,17 @@ def random_timetables(im):
         else:
         	break
 
-    while True:
-        overlap_input = input("Can subjects be placed in the same timeslot? y/n: ")
-        
-        if overlap_input != "y" and overlap_input != "n":
-        	print("Invalid input")
-        	continue
-        elif overlap_input == "y":
-        	no_overlap = False
-        	print("Overlap allowed.")
-        	break
-        elif overlap_input == "n":
-   	    	no_overlap = True
-   	    	print("No overlap allowed.")
-   	    	break
+    overlap_input = input("Can subjects be placed in the same timeslot? y/n: ")
+    
+    if overlap_input == "y":
+    	no_overlap = False
+    	print("Overlap allowed.")
+    elif overlap_input == "n":
+    	no_overlap = True
+    	print("No overlap allowed.")
+    else:
+    	no_overlap = True
+    	print("Invalid input. No overlap allowed by default.")
 
     print("Starting random timetables with %s iterations..."
             % nPlannedIterations)
