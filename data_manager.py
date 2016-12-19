a65415f4c346e2d253c8764f96738e5ae24e1a37
MangoDream1 <axel.verheul@gmail.com>
2016-12-19 15:55:14 +0100

    Finished commenting data_manager

--- data_manager.py
+++ data_manager.py
@@ -5,7 +5,13 @@ from random import randint, choice
 from operator import itemgetter
 
 class DataManager:
+    ''' The DataManager class contains all the data needed for the algorithms.
+        Also contains several helpfull functions that are used by the
+        algorithms'''
+
     def __init__(self, classrooms, subjects, students):
+        # Saves changes of all previous iterations. Doesnt save whole timetables
+        # only the changed lectures
         self.iteration_dct = {}
 
         self.classrooms = classrooms
@@ -24,43 +30,55 @@ class DataManager:
         for subject in self.subjects:
             subject.assignStudentsToLectures()
 
-        # Two way dictionary for finding lectures
+        # Two way dictionary for finding lectures and the lecture index
         self.lectures = [lecture for subject in subjects for lecture in subject.lectures]
         self.lecture_dct = {index: lecture for index, lecture in enumerate(self.lectures)}
         self.lecture_dct = dict(self.lecture_dct.items() | dict(reversed(item) for item in self.lecture_dct.items()).items())
 
+        # The current iteration
         self.i = 0
 
+        # New instance of plot and score system
         self.score_system = ScoreSystem(self)
         self.plot = Plot()
 
+
     def resetLectures(self):
+        # Clear all lectures from subjects, students and classrooms
         for x in self.subjects + self.students + self.classrooms:
             x.clearLectures()
 
+        # Append all lectures to correct subject, students and classroom
         for x in self.lectures:
             x.assignLecturetoAll()
 
+
     def resetTimetables(self):
+        # Clear timetable for all
         for x in self.subjects + self.students + self.classrooms:
             x.clearTimetable()
 
+
     def addChanges(self, changed_lectures, withStudents=False):
+        #Add dictionary of lectures index as key and changingDataDict as value
         self.iteration_dct[self.i] = {self.lecture_dct[l]:l.getChangingDataDict()
                                         for l in changed_lectures}
 
+        # if withStudents also add students to changes
         if withStudents:
             for l in changed_lectures:
                 dct = self.iteration_dct[self.i][self.lecture_dct[l]]
                 dct["students"] = [s.getId() for s in l.students]
 
-
+        # Base default false
         self.iteration_dct[self.i]["base"] = False
 
+        # Reset lectures then calculate score and add to dict
         self.resetLectures()
         self.iteration_dct[self.i]["score"] = self.score_system.total_score()
 
         return self.iteration_dct
+
 
     def compileChanges(self, i):
         # Find nearest base
@@ -70,6 +88,8 @@ class DataManager:
                 base = i
                 break
 
+        # From base add all changes until i
+        # This is the compiled result
         compiled_result = {}
         for x in range(base, i+1):
             for key, data in self.iteration_dct[x].items():
@@ -77,13 +97,16 @@ class DataManager:
 
         return compiled_result
 
+
     def createBase(self):
+        # Create a base by compiling all to this point
         self.iteration_dct[self.i] = self.compileChanges(self.i)
         self.iteration_dct[self.i]["base"] = True
 
-        return self.iteration_dct
 
     def applyChanges(self, compiled_changes):
+        # Change the values of the current lecture object so that it matches
+        # the compiled changes
         for index, data in compiled_changes.items():
             if index not in ["base", "score"]:
                 lecture = self.lecture_dct[index]
@@ -102,9 +125,13 @@ class DataManager:
 
         return self.lecture_dct
 
-    def randomLocation(self, lecture, no_overlap=False):
-        overlap = True
 
+    def randomLocation(self, lecture, no_overlap=False):
+        # Find a random location for a lecture. If no_overlap is True
+        # keep looking until a space has been found where no other lectures
+        # are.
+
+        overlap = True
         while overlap:
             lecture.day = randint(0, 4)
             lecture.timeslot = randint(0, 3)
@@ -123,7 +150,10 @@ class DataManager:
 
         return lecture
 
+
     def removeOverlap(self):
+        # Go through the timetables to find overlap and remove it randomly
+
         for x in self.classrooms:
             x.fillInTimetable()
 
@@ -141,7 +171,10 @@ class DataManager:
         for x in self.classrooms + self.students + self.subjects:
             x.fillInTimetable()
 
+
     def compileBest(self):
+        # Find the best score in iteration_dct and compile it
+
         best_iteration, score = max([(i, self.iteration_dct[i]["score"])
                                 for i in self.iteration_dct.keys()],
                                 key=itemgetter(1))
@@ -151,7 +184,11 @@ class DataManager:
 
         return best_iteration, score
 
+
     def exportLectures(self, file_name, plot=True):
+        # Export the current timetable to file_name. Exports timetable, score
+        # distribution and a plot if plot=True
+
         export_dct = [x.toLongDict() for x in self.lectures]
 
         if not os.path.exists("Timetable/Lectures"):
@@ -169,8 +206,10 @@ class DataManager:
         with open("Timetable/Lectures/%s.json" % file_name, 'w') as f:
             json.dump(export_dct, f, indent=3)
 
+        # Recalculate score
         self.score_system.total_score()
 
+        # The score dct that is going to be saved
         score_dct = {"classrooms":
                         {x.getId(): x.score for x in self.classrooms},
                      "subjects":
@@ -192,14 +231,20 @@ class DataManager:
         with open("Timetable/Scores/%s.json" % file_name, 'w') as f:
             json.dump(score_dct, f, indent=3)
 
+        # Export plot
         if plot:
             self.plot.plotTime(file_name)
 
+
     def exportTimetable(self):
+        # Export all the objects with timetables so it can be read by webview
         for x in self.subjects + self.students + self.classrooms:
             x.exportTimetable()
 
+
     def importLectures(self, file_name):
+        # Import lecture file and apply contents
+
         file_name = file_name.replace(".json", "")
         path = "Timetable/Lectures/%s.json" % file_name
 
