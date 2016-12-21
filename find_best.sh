0832c80d5731293dcf69f9af73f06336935c3461
MangoDream1 <axel.verheul@gmail.com>
2016-12-21 17:15:18 +0100

    Echo start time and echo end time strings

--- find_best.sh
+++ find_best.sh
@@ -1,12 +1,14 @@
 # Save start time
-date > find_best_time.txt
+echo "Start time:" > find_best_time.txt
+date >> find_best_time.txt
 
 # Run all python commands
-python3 run4.py -i 500 -n 100 # Start off with 500 swap hill climbers with a noProgressLimit of 100
-python3 run4.py -i 100 -p # Improve the best 100 with a noProgressLimit of 100
-python3 run4.py -i 12 -p -n 10000 # Improve 12 again but with nPl of 10000
-python3 run4.py -i 4 -p -n 100000 # Improve 4 again but with nPl of 100000
+python3 run4.py -i 500 -n 100 && # Start off with 500 swap hill climbers with a noProgressLimit of 100
+python3 run4.py -i 100 -p && # Improve the best 100 with a noProgressLimit of 100
+python3 run4.py -i 12 -p -n 10000 && # Improve 12 again but with nPl of 10000
+python3 run4.py -i 4 -p -n 100000 && # Improve 4 again but with nPl of 100000
 python3 run4.py -i 4 -p -s -n 10000 # Improve with student optimizatioon with nPl of 100000
 
 # Save end time
+echo "End time:" >> find_best_time.txt
 date >> find_best_time.txt
