586dd364be103c657c277fde0e4d7efc86f8d8b3
MangoDream1 <axel.verheul@gmail.com>
2016-12-22 12:59:34 +0100

    Fixed spelling mistake

--- find_best.sh
+++ find_best.sh
@@ -4,27 +4,32 @@ date >> find_best_time.txt
 
 # Run all python commands
 
-python3 run4.py -i 500 -n 100 && # Start off with 500 swap hill climbers with a noProgressLimit of 100
+# Start off with 500 swap hill climbers with a noProgressLimit of 100
+python3 run4.py -i 500 -n 100 &&
 
 echo "Start stage 2:" >> find_best_time.txt
 date >> find_best_time.txt
 
-python3 run4.py -i 100 -p && # Improve the best 100 with a noProgressLimit of 100
+# Improve the best 100 with a noProgressLimit of 100
+python3 run4.py -i 100 -p &&
 
 echo "Start stage 3:" >> find_best_time.txt
 date >> find_best_time.txt
 
-python3 run4.py -i 24 -p -n 10000 && # Improve 24 again but with nPl of 10000
+# Improve 24 again but with nPl of 10000
+python3 run4.py -i 24 -p -n 10000 &&
 
 echo "Start stage 4:" >> find_best_time.txt
 date >> find_best_time.txt
 
-python3 run4.py -i 8 -p -n 100000 && # Improve 8 again but with nPl of 100000
+# Improve 8 again but with nPl of 100000
+python3 run4.py -i 8 -p -n 100000 &&
 
 echo "Start stage 5:" >> find_best_time.txt
 date >> find_best_time.txt
 
-python3 run4.py -i 4 -p -s -n 10000 # Improve with student optimizatioon with nPl of 100000
+# Improve with student optimization with nPl of 100000
+python3 run4.py -i 4 -p -s -n 10000
 
 # Save end time
 echo "End time:" >> find_best_time.txt
